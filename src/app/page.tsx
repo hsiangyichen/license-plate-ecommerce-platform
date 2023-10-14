cd214318a74694b98e2b8414927fdf54d48dfb0b
hsiangyichen <hsiangyi1025@gmail.com>
2023-10-14 05:35:27 -0700

    feat: complete the design of home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Link from "next/link";
-import { Navbar, AboutUs } from "@components";
+import { Navbar, WhyChooseUs } from "@components";
 import React from "react";
 import styles from "./styles.module.scss";
 import ArrowRightIcon from "@assets/icons/arrow-right-small.svg";
@@ -11,138 +11,71 @@ export default function Home() {
   const executeScroll = () => topRef.current?.scrollIntoView();
 
   return (
-    <div className={styles.container}>
-      <video
-        autoPlay
-        muted
-        loop
-        className={styles.video}
-        src={"/videos/men.mp4"}
-      />
-      <div
-        style={{
-          background:
-            "linear-gradient(90deg, rgba(40, 54, 48, 0.00) 0%, rgba(32, 46, 42, 0.00) 16.67%, rgba(16, 75, 71, 0.37) 34.38%, rgba(16, 75, 71, 0.70) 70.83%, rgba(16, 75, 71, 0.70) 100%)",
-          objectFit: "cover",
-          objectPosition: "center",
-          position: "absolute",
-          right: "0",
-          width: "60%",
-          height: "100vh",
-        }}
-      />
-      <div className={styles.item}>
-        <Navbar />
-        <div className={styles.itemdown}>
-          <div className={styles.title}>
-            <h1 style={{ fontSize: "120px", fontWeight: "800" }}>
-              Find Your Desired Car Plates <br />
-            </h1>
-            <p style={{ fontSize: "24px", fontWeight: "300" }}>
-              Find Your Lucky Plate Number
-            </p>
-          </div>
+    <div>
+      <div className={styles.container}>
+        <img className={styles.homeImg} src={"images/home2.jpg"} />
+        <div className={styles.item}>
+          <Navbar />
+          <div className={styles.itemdown}>
+            <div className={styles.left}>
+              <Link href="/purchase">
+                <button className={styles.button}>選擇專屬於你的車牌</button>
+              </Link>
+            </div>
 
-          <div className={styles.right}>
-            <p className={styles.desc}>
-              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&quot;218
-              幸運車牌&quot;
-              是一家專業的車牌買賣公司，提供全面的車牌交易服務。218致力於讓車牌交易變得簡單、快捷和安全。無論您是想買一個獨特的車牌號碼，還是需要出售或轉讓您現有的車牌，我們的團隊將全程協助您完成交易手續。
-            </p>
-            <Link href="/purchase">
-              <button className={styles.button}>查看所有車牌</button>
-            </Link>
+            <div className={styles.right}>
+              <h1>
+                218
+                <br />
+                你的專屬車牌
+              </h1>
+              {/* <p style={{ fontSize: "24px", fontWeight: "300" }}>
+              Find Your Lucky Plate Number
+            </p> */}
+            </div>
           </div>
+          <a onClick={executeScroll} className={styles.downArrow}>
+            <ArrowDownIcon color="white" />
+          </a>
         </div>
-        <a onClick={executeScroll} className={styles.downArrow}>
-          <ArrowDownIcon color="white" />
-        </a>
+        <div className={styles.homeFilter} />
       </div>
       <div id="aboutus" ref={topRef} className={styles.aboutusContainer}>
         <div className={styles.aboutusLeft}>
-          <div
-            style={{
-              width: "100%",
-              height: "50vh",
-              border: "2px solid gray",
-              top: "20px",
-              marginLeft: "20px",
-              position: "absolute",
-            }}
-          ></div>
-          <div className={styles.leftTop}>
-            <h2 style={{ fontSize: "25px", fontWeight: "300" }}>
-              ABOUT US / 關於我們
-            </h2>
-            <h1 style={{ fontSize: "45px", fontWeight: "600" }}>Our Service</h1>
-            <Link href="/about" className={styles.moreLink}>
-              <span
-                style={{
-                  margin: "0 8px",
-                }}
-              >
-                See More
-              </span>
-
-              <ArrowRightIcon className={styles.arrowIcon} />
-            </Link>
-          </div>{" "}
-          <div className={styles.leftBottom}>
-            <p
+          <h2>ABOUT US / 關於我們</h2>
+          <h1>Our Service</h1>
+          <p>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&quot;218
+            幸運車牌&quot;
+            是一家專業的車牌買賣公司，無論您是想一個獨特的車牌號碼，還是需要出售或轉讓您現有的車牌，我們的團隊將全程協助您完成交易手續。
+            <br />
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;我們以透明、誠信和專業聞名，並致力於超越客戶的期望，為客戶提供「
+            優質的服務和滿意的交易 」結果是我們的使命。
+          </p>
+          <Link href="/about" className={styles.moreLink}>
+            <span
               style={{
-                fontSize: "18px",
-                fontWeight: "100",
+                margin: "0 8px",
               }}
             >
-              我們以透明、誠信和專業聞名，並致力於超越客戶的期望，
-              <br />
-              <br />
-              <br />
-              為客戶提供「 優質的服務和滿意的交易 」結果是我們的使命。
-            </p>
-          </div>
-        </div>
+              See More
+            </span>
 
-        <div
-          style={{
-            width: "100%",
-          }}
-        >
-          <img
-            style={{
-              height: "100vh",
-              width: "100%",
-              objectFit: "cover",
-              objectPosition: "center",
-            }}
-            src={"images/car3.jpg"}
-          />
+            <ArrowRightIcon className={styles.arrowIcon} />
+          </Link>
+        </div>
+        <div className={styles.aboutusRight}>
+          <img className={styles.car3} src={"images/car3.jpg"} />
+          <img className={styles.car2} src={"images/car2.jpg"} />
+          <img className={styles.car1} src={"images/car1.jpg"} />
         </div>
       </div>
-      <AboutUs />
-      <div
-        style={{
-          width: "100%",
-          backgroundColor: "rgb(42, 45, 50)",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          textAlign: "center",
-          fontWeight: "300",
-        }}
-      >
-        <p
-          style={{
-            fontSize: "18px",
-            padding: "80px 250px",
-            margin: "50px",
-            color: "white",
-            border: "1.5px solid white",
-            // backgroundColor: "rgb(42, 45, 50)",
-          }}
-        >
+      <WhyChooseUs />
+      <div className={styles.navigate}>
+        <p>
           請瀏覽我們的網頁了解更多公司和服務信息。有任何問題或需要幫助，請隨時聯繫我們團隊。
-          <br /> 感謝您選擇我們，期待為您提供卓越的車牌交易體驗！
+          <br />
+          感謝您選擇我們，期待為您提供卓越的車牌交易體驗！
         </p>
       </div>
     </div>
--- src/components/index.ts
+++ src/components/index.ts
@@ -2,7 +2,7 @@ export * from "./Footer";
 export * from "./Navbar";
 export * from "./SearchBar";
 export * from "./SearchResult";
-export * from "./AboutUs";
+export * from "./WhyChooseUs";
 export * from "./SortByDropdown";
 export * from "./ScrollToTopButton";
 export * from "./Pagination";
