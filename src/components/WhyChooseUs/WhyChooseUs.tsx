6baa95d2a91b77724f6684a945920bb935fa2e91
hsiangyichen <hsiangyi1025@gmail.com>
2023-10-21 04:17:30 -0700

    feat: add a filter in home page and icons

--- src/components/WhyChooseUs/WhyChooseUs.tsx
+++ src/components/WhyChooseUs/WhyChooseUs.tsx
@@ -4,6 +4,9 @@ import styles from "./styles.module.scss";
 import LicensePlateIcon from "@assets/icons/license-plate.svg";
 import ToolsIcon from "@assets/icons/tools.svg";
 import WalletIcon from "@assets/icons/wallet.svg";
+import LicensePlateIconMobile from "@assets/icons/license-plate-mobile.svg";
+import ToolsIconMobile from "@assets/icons/tools-mobile.svg";
+import WalletIconMobile from "@assets/icons/wallets-mobile.svg";
 
 const WhyChooseUs = () => {
   return (
@@ -17,20 +20,29 @@ const WhyChooseUs = () => {
           </p>
         </div>
         <section className={styles.section}>
-          <div>
-            <LicensePlateIcon />
-            <h2>豐富車牌庫</h2>
-            <p>我們提供各種多樣的車牌選項，以滿足您獨特的偏好。</p>
+          <div className={styles.sectionItem}>
+            <LicensePlateIcon className={styles.icon} />
+            <LicensePlateIconMobile className={styles.iconMobile} />
+            <div>
+              <h2>豐富車牌庫</h2>
+              <p>我們提供各種多樣的車牌選項，以滿足您獨特的偏好。</p>
+            </div>
           </div>
-          <div>
-            <WalletIcon />
-            <h2>價格相宜</h2>
-            <p>我們的價格公平合理，並根據您的預算提供最佳的選擇。</p>
+          <div className={styles.sectionItem}>
+            <WalletIcon className={styles.icon} />
+            <WalletIconMobile className={styles.iconMobile} />
+            <div>
+              <h2>價格相宜</h2>
+              <p>我們的價格公平合理，並根據您的預算提供最佳的選擇。</p>
+            </div>
           </div>
-          <div>
-            <ToolsIcon />
-            <h2>優質服務</h2>
-            <p>我們經驗豐富的團隊確保您獲得 專業和滿意的服務。</p>
+          <div className={styles.sectionItem}>
+            <ToolsIcon className={styles.icon} />
+            <ToolsIconMobile className={styles.iconMobile} />
+            <div>
+              <h2>優質服務</h2>
+              <p>我們經驗豐富的團隊確保您獲得 專業和滿意的服務。</p>
+            </div>
           </div>
         </section>
       </div>
